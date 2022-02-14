165154725df71ad625ef2af7161bd8b34b933f81
yejinleee <yejin9487@gmail.com>
2022-02-14 16:51:07 +0900

    socket추가 시도

---
+++ app.js
@@ -0,0 +1,84 @@
+const socket = io('http://127.0.0.1:3000');
+
+const welcome = document.querySelector("#welcome")
+const form = welcome.querySelector("form");
+const room = document.querySelector("#room");
+
+room.hidden=true;
+
+let nickname = 'annonymous';
+function addMessage(message){
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li")
+    li.innerText = message;
+    ul.appendChild(li);
+}
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#nickname input");
+    const value = input.value;
+    socket.emit("nicknameSave", value);
+    nickname = input.value;
+    const nickForm = room.querySelector("#nickname");
+    nickForm.hidden = true;
+    const h4 = room.querySelector("h4");
+    h4.innerText=`Hi! ${nickname}`;
+}
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#message input");
+    const value = input.value;
+    socket.emit("new_message", value, roomname, ()=>{ //어느방인지도 알려줘야해서 roomName도 보내야함  //백으로 보냄
+        addMessage(`${nickname} :  ${value}`)
+    });
+    input.value="";
+}
+let roomname;
+function showRoom(){
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room : ${roomname}`;
+    const nicknameForm = room.querySelector("#nickname");
+    const messageForm = room.querySelector("#message");
+    nicknameForm.addEventListener("submit",handleNicknameSubmit);
+    messageForm.addEventListener("submit",handleMessageSubmit);
+}
+function handleRoomSubmit(event){
+    console.log(event);
+    event.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("enter_room", input.value, showRoom); 
+    roomname = input.value;
+    input.value = "";
+}
+form.addEventListener("submit",handleRoomSubmit);
+
+
+socket.on("welcome",(user,newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room : ${roomname} (${newCount})`;
+    addMessage(`${user} arrived!`)
+});
+socket.on("bye",(userLeft, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room : ${roomname} (${newCount})`;
+    addMessage(`${userLeft} left...`)
+});
+socket.on("new_message", addMessage);
+
+// socket.on("room_change",console.log); //(msg)=>{console.log(msg)} 랑 같은거
+socket.on("room_change", (rooms ) =>{
+    // 근데이제 이게 실행됐을때 rooms가 비어있는 값이면 그전 값을 바꾸진 않음
+    // 그래서 확인 필요!
+    if (rooms.length ==0){
+        reuturn
+    }
+    const roomList = welcome.querySelector("ul");
+    roomList.innerText=""; //항상 다 비웠다가 시작
+    rooms.forEach( (room) =>{
+        const li = document.createElement("ll");
+        li.innerText= room;
+        roomList.append(li);
+    })
+});
---
+++ server.js
@@ -0,0 +1,73 @@
+import express from "express";
+import http from "http"; //node.js에 내장되어있어서 설치필요X 
+// import {WebSocketServer} from "ws";
+import {Server} from "socket.io";
+import {instrument} from "@socket.io/admin-ui";
+
+const app = express();
+const handleListen = ()=>console.log(`Listening on http://localhost:3000`)
+
+//http 서버 
+const server = http.createServer(app); //app.listen안해도 서버에 접근할 수 있게 서버 만듦.
+// //ws서버
+// const wss = new WebSocketServer({server }); 
+// socketIO
+const wsServer = new Server(server,{
+    cors: {
+      origin: ["https://admin.socket.io"],
+      credentials: true
+    }
+  });
+  instrument(wsServer, {
+    auth: false
+  });
+  
+function publicRooms(){
+    const {
+        sockets: {
+            adapter : {sids, rooms},
+        },
+    } = wsServer;
+
+    const publicRooms = [];
+    rooms.forEach((_,key) =>{
+        if (sids.get(key) === undefined){
+            publicRooms.push(key);
+        }
+    })
+    return publicRooms;
+}
+
+function countRoom(roomName){
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
+wsServer.on("connection", (socket) =>{
+    socket["nickname"] = 'Annonymous';
+    socket.onAny((event)=>{
+        console.log('발생한 Socket Event : ',event);
+    })
+    socket.on("enter_room", (roomName,done) =>{
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+        wsServer.sockets.emit("room_change",publicRooms());
+    })
+    socket.on("disconnecting", ()=>{ //disconnecting : 서버와 연결이 끊어지기 직전에 발생시킴
+        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname, countRoom(room)-1 ));
+        // 연결한 모든 방에 대해 bye라는  이벤트 보내는거
+    })
+    socket.on("disconnect", ()=>{ //연결 끊긴 후 발생
+        wsServer.sockets.emit("room_change",publicRooms());
+    })
+    socket.on("new_message", ( msg, room, done)=>{
+        socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`); //여기 new_message는 바로위 new_message랑 다른 지칭인데. 이렇게 같은 단어여도 된다는것
+        done(); //실행은 프론트!
+    })
+    socket.on("nicknameSave", (nickname) => {
+        socket["nickname"]=nickname;
+
+    });
+})
+
+server.listen(3000,handleListen);
